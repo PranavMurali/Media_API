451a19f41a663b9197d323ac9f23cd255d8958f2
Pranav Murali <pranavmurali2001@gmail.com>
2021-10-09 00:59:56 +0530

    :+1: users and posting done on API

--- main.go
+++ main.go
@@ -1,17 +1,126 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
-type User struct {
+var client *mongo.Client
+
+type Person struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" bson:"password,omitempty"`
+}
+
+type Post struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	FirstName string             `json:"firstName,omitempty" bson:"firstName,omitempty"`
-	LastName  string             `json:"lastName,omitempty" bson:"lastName,omitempty"`
+	UserID    string             `json:"userid,omitempty" bson:"userid,omitempty"`
+	Caption   string             `json:"caption,omitempty" bson:"caption,omitempty"`
+	Image     string             `json:"image,omitempty" bson:"image,omitempty"`
+	Timestamp string             `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
+}
+
+func CreatePersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var person Person
+	_ = json.NewDecoder(request.Body).Decode(&person)
+	crypts, err := bcrypt.GenerateFromPassword([]byte(person.Password), 8)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	person.Password = string(crypts)
+	collection := client.Database("SocialMedia").Collection("Users")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, person)
+	json.NewEncoder(response).Encode(result)
+}
+
+func GetPersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var person Person
+	collection := client.Database("SocialMedia").Collection("Users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, Person{ID: id}).Decode(&person)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(person)
+}
+
+func CreatePostEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var post Post
+	_ = json.NewDecoder(request.Body).Decode(&post)
+	timestamp := time.Now().UTC().Format(time.RFC3339)
+	post.Timestamp = timestamp
+	collection := client.Database("SocialMedia").Collection("Posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, post)
+	json.NewEncoder(response).Encode(result)
+}
+
+func GetPostEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var post Post
+	collection := client.Database("SocialMedia").Collection("Posts")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, Post{ID: id}).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(post)
+}
+
+func GetUserPostEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	var post Post
+	collection := client.Database("SocialMedia").Collection("Posts")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, Post{UserID: params["id"]}).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(post)
 }
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Starting the API...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	godotenv.Load(".env")
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URL"))
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
+	router.HandleFunc("/users", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/users/{id}", GetPersonEndpoint).Methods("GET")
+
+	router.HandleFunc("/posts", CreatePostEndpoint).Methods("POST")
+	router.HandleFunc("/posts/{id}", GetPostEndpoint).Methods("GET")
+
+	router.HandleFunc("/posts/users/{id}", GetUserPostEndpoint).Methods("GET")
+	http.ListenAndServe(":10533", router)
 }
